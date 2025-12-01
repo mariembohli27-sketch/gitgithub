1b38327df9d548007126f216fd3c3ae4f96a31bd
mariembohli27 <mariembohli27@gmail.com>
2025-12-01 18:47:47 +0100

    update main

--- src/main/java/com/example/gitgithub/file1.java
+++ src/main/java/com/example/gitgithub/file1.java
@@ -66,6 +66,7 @@ package com.example.gitgithub;
 public class file1 {
     private String message = "Hello, GitHub!";
     private String author = "Mohamed Yassine Derbel";
+    private string version="1.0.0";
 
     public String getMessage() {
         return message;
